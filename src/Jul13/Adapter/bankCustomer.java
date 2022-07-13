4269f0b782073b3941997a4a0aaea25aa23eebd1
Tanay Kapoor <itstkaay@gmail.com>
2022-07-13 13:17:27 +0530

    Created files

---
+++ src/Jul13/Adapter/bankCustomer.java
@@ -0,0 +1,14 @@
+package Jul13.Adapter;
+
+public class bankCustomer extends bankDetails implements creditCard {
+
+    @Override
+    public void getBankDetails() {
+        
+    }
+
+    @Override
+    public void getCreditCardDetails() {
+
+    }
+}
