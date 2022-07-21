f042212c91a1e8f683a286a3a6025a3e39dc8212
Tanay Kapoor <itstkaay@gmail.com>
2022-07-21 12:11:05 +0530

    Create staticNestedClass.java

---
+++ src/Jul21/staticNestedClass.java
@@ -0,0 +1,28 @@
+package Jul21;
+
+class outerClass {
+    static int i = 10;
+    int j = 20;
+    private int k = 30;
+
+    public void display() {
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+    }
+
+    static class innerClass {
+        void display() {
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+public class staticNestedClass {
+    public static void main(String[] args) {
+        outerClass obj = new outerClass();
+        outerClass.innerClass obj1 = new outerClass.innerClass();
+        obj.display();
+        obj1.display();
+    }
+}
